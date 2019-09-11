6052833030860668b461a1491b4997988f41fa78
rayah <rayah.khatib.2@gmail.com>
2019-09-11 14:48:16 +0300

    Assignment done and moved

--- src/main/java/com/grauman/amdocs/controllers/AssignmentsController.java
+++ src/main/java/com/grauman/amdocs/controllers/AssignmentsController.java
@@ -1,10 +1,31 @@
 package com.grauman.amdocs.controllers;
 
 
+import com.grauman.amdocs.dao.AssignmentsDAO;
+import com.grauman.amdocs.errors.custom.ResultsNotFoundException;
+import com.grauman.amdocs.models.AssignmentHistory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/assignments")
 public class AssignmentsController {
+    @Autowired
+    private AssignmentsDAO assignmentsDAO;
+
+    @GetMapping("")
+    public ResponseEntity<List<AssignmentHistory>> getAssignmentsHistoryForEmployee(@RequestParam int employeeId, @RequestParam int pageNumber, @RequestParam int limit) throws SQLException {
+        System.out.println("aaaaaaaaaaaaa");
+        List<AssignmentHistory> assignments = assignmentsDAO.getAssignmentsByUserID(employeeId, pageNumber, limit);
+//        return ResponseEntity.ok().body(assignments);
+        return new ResponseEntity<>(assignments, HttpStatus.OK);
+    }
 }
--- src/main/java/com/grauman/amdocs/dao/AssignmentsDAO.java
+++ src/main/java/com/grauman/amdocs/dao/AssignmentsDAO.java
@@ -1,12 +1,23 @@
 package com.grauman.amdocs.dao;
 
 import com.grauman.amdocs.dao.interfaces.IAssignmentsDAO;
+import com.grauman.amdocs.errors.custom.ResultsNotFoundException;
 import com.grauman.amdocs.models.Assignment;
+import com.grauman.amdocs.models.AssignmentHistory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-
+@Service
 public class AssignmentsDAO implements IAssignmentsDAO {
+    @Autowired
+    DBManager db;
+
     @Override
     public List<Assignment> findAll() throws SQLException {
         return null;
@@ -30,5 +41,49 @@ public class AssignmentsDAO implements IAssignmentsDAO {
     @Override
     public Assignment delete(int id) throws SQLException {
         return null;
+    }
+
+    @Override
+    public List<AssignmentHistory> getAssignmentsByUserID(int employeeID, int currPage, int limit) throws SQLException {
+        List<AssignmentHistory> assignments = new ArrayList<AssignmentHistory>();
+
+        if (currPage < 1)
+            currPage = 1;
+
+        int offset = (currPage - 1) * limit; // index of which row to start retrieving data
+
+        try (Connection conn = db.getConnection()) {
+
+            String sqlCommand = "Select a.id, project_id, p.name, a.start_date, a.end_date, a.status, a.requested_from_manager_id,a.requested_to_manager_id\n" +
+                                 "from assignment a join project p on a.project_id=p.id where employee_id = ? limit ? offset ?";
+
+            try (PreparedStatement command = conn.prepareStatement(sqlCommand)) {
+                command.setInt(1, employeeID);
+                command.setInt(2, limit);
+                command.setInt(3, offset);
+
+                try (ResultSet result = command.executeQuery()) {
+                    while (result.next()) {
+                        assignments.add(new AssignmentHistory(
+                                result.getInt("a.id"),
+                                result.getString("p.name"),
+                                result.getInt("a.project_id"),
+                                employeeID,
+                                result.getDate("a.start_date"),
+                                result.getDate("a.end_date"),
+                                result.getInt("a.requested_from_manager_id"),
+                                result.getInt("a.requested_to_manager_id"),
+                                result.getString("a.status"))
+                        );
+                    }
+                }
+            }
+
+        }
+        if (assignments.isEmpty()) {
+            throw new ResultsNotFoundException("Couldn't find assignments for this employee");
+
+        }
+        return assignments;
     }
 }
