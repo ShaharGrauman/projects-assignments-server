03eeba7ccc8bcf1865b52a53fc94d013fff7bfa8
Lama8 <lama.azaiza@gmail.com>
2019-09-16 15:42:30 +0300

    add Cross Origin annotation

--- src/main/java/com/grauman/amdocs/controllers/AssignmentsController.java
+++ src/main/java/com/grauman/amdocs/controllers/AssignmentsController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/assignments")
+@CrossOrigin
 public class AssignmentsController {
     @Autowired
     private AssignmentsDAO assignmentsDAO;
--- src/main/java/com/grauman/amdocs/controllers/WorkSiteController.java
+++ src/main/java/com/grauman/amdocs/controllers/WorkSiteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.grauman.amdocs.models.WorkSite;
 
 @RestController
 @RequestMapping("/worksite")
+@CrossOrigin
 public class WorkSiteController {
     
     @Autowired
