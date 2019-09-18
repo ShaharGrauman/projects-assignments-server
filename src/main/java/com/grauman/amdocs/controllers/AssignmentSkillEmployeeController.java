efca4ecabe851b1ad7dddacf2756562dc91de110
rayah <rayah.khatib.2@gmail.com>
2019-09-18 11:20:35 +0300

    fixed paths

--- src/main/java/com/grauman/amdocs/controllers/AssignmentSkillEmployeeController.java
+++ src/main/java/com/grauman/amdocs/controllers/AssignmentSkillEmployeeController.java
@@ -6,11 +6,9 @@ import com.grauman.amdocs.models.vm.AssignmentSkillEmployeeVM;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -20,33 +18,33 @@ public class AssignmentSkillEmployeeController {
     @Autowired
     private AssignmentSkillEmployeeDAO employeeDAO;
 
-    @GetMapping("")
-    public ResponseEntity<List<AssignmentSkillEmployeeVM>> getEmployeesByManagerID(@RequestParam Integer managerID, @RequestParam Integer pageNumber, @RequestParam Integer limit) throws SQLException {
+    @GetMapping("/{id}")
+    public ResponseEntity<List<AssignmentSkillEmployeeVM>> getEmployeesByManagerID(@PathVariable("id") Integer managerID, @RequestParam Integer pageNumber, @RequestParam Integer limit) throws SQLException {
         List<AssignmentSkillEmployeeVM> employees = employeeDAO.getEmployeesByManagerID(managerID, pageNumber,limit);
         return ResponseEntity.ok().body(employees);
     }
 
-    @GetMapping("/projectid")
-    public ResponseEntity<List<AssignmentSkillEmployeeVM>> getEmployeesByProjectID(@RequestParam Integer projectid) throws SQLException {
+    @GetMapping("/project/{id}")
+    public ResponseEntity<List<AssignmentSkillEmployeeVM>> getEmployeesByProjectID(@PathVariable("id") Integer projectid) throws SQLException {
         List<AssignmentSkillEmployeeVM> employees = employeeDAO.getEmployeesByProjectID(projectid);
         return ResponseEntity.ok().body(employees);
 
     }
 
-    @GetMapping("/skill")
-    public ResponseEntity<List<AssignmentSkillEmployeeVM>> searchEmployeesBySkillID(@RequestParam Integer skillID, @RequestParam Integer pageNumber, @RequestParam Integer limit) throws SQLException {
+    @GetMapping("/skill/{id}")
+    public ResponseEntity<List<AssignmentSkillEmployeeVM>> searchEmployeesBySkillID(@PathVariable("id") Integer skillID, @RequestParam Integer pageNumber, @RequestParam Integer limit) throws SQLException {
         List<AssignmentSkillEmployeeVM> employees = employeeDAO.searchEmployeesBySkillID(skillID, pageNumber,limit);
         return ResponseEntity.ok().body(employees);
 
     }
-    @GetMapping("/skillname")
-    public ResponseEntity<List<AssignmentSkillEmployeeVM>> searchEmployeesBySkillName(@RequestParam String skillName, @RequestParam Integer pageNumber, @RequestParam Integer limit) throws SQLException {
+    @GetMapping("/skill/{name}")
+    public ResponseEntity<List<AssignmentSkillEmployeeVM>> searchEmployeesBySkillName(@PathVariable("name") String skillName, @RequestParam Integer pageNumber, @RequestParam Integer limit) throws SQLException {
         List<AssignmentSkillEmployeeVM> employees = employeeDAO.searchEmployeesBySkillName(skillName, pageNumber,limit);
         return ResponseEntity.ok().body(employees);
 
     }
-    @GetMapping("/skillset")
-    public ResponseEntity<List<AssignmentSkillEmployeeVM>> searchEmployeesBySkillSet(@RequestParam List<Integer> skillSet, @RequestParam Integer pageNumber, @RequestParam Integer limit) throws SQLException{
+    @GetMapping("/skill/{set}")
+    public ResponseEntity<List<AssignmentSkillEmployeeVM>> searchEmployeesBySkillSet(@PathVariable("set") List<Integer> skillSet, @RequestParam Integer pageNumber, @RequestParam Integer limit) throws SQLException{
         List<AssignmentSkillEmployeeVM> employees = employeeDAO.searchEmployeesBySkillSet(skillSet, pageNumber,limit);
         return ResponseEntity.ok().body(employees);
 
