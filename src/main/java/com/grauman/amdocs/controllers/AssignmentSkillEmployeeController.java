eabb8081d0548711857b3fa9900038a3c5fc1977
rayah <rayah.khatib.2@gmail.com>
2019-09-18 11:25:13 +0300

    fixed paths

--- src/main/java/com/grauman/amdocs/controllers/AssignmentSkillEmployeeController.java
+++ src/main/java/com/grauman/amdocs/controllers/AssignmentSkillEmployeeController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/team")
+@CrossOrigin
 public class AssignmentSkillEmployeeController {
     @Autowired
     private AssignmentSkillEmployeeDAO employeeDAO;
