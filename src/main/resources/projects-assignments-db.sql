e6b48debcb9b65af8c933c4a095ef64e92c298ac
Shahar Grauman <info@grauman.co.il>
2019-09-20 14:11:44 +0300

    Fixing schema

--- src/main/resources/projects-assignments-db.sql
+++ src/main/resources/projects-assignments-db.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `assignments` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `assignments` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
 USE `assignments`;
--- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: assignments
 -- ------------------------------------------------------
--- Server version	8.0.16
+-- Server version	8.0.12
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,37 +20,30 @@ USE `assignments`;
 --
 -- Table structure for table `assignment`
 --
+
 DROP TABLE IF EXISTS `assignment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-CREATE TABLE assignment (
- id int(11) NOT NULL AUTO_INCREMENT,
- project_id int(11) NOT NULL,
- employee_id int(11) NOT NULL,
- start_date date NOT NULL,
- end_date date NOT NULL,
- requested_from_manager_id int(11) NOT NULL,
- requested_to_manager_id int(11) DEFAULT NULL,
- status enum('Pending approval','Not approved','In progress','Done!')  NOT NULL,
- PRIMARY KEY (id),
- KEY employee_id (employee_id),
- KEY project_id (project_id),
- KEY requested_from_manager_id (requested_from_manager_id),
- KEY requested_to_manager_id (requested_to_manager_id),
- CONSTRAINT assignment_ibfk_1 FOREIGN KEY (employee_id) REFERENCES users (id),
- CONSTRAINT assignment_ibfk_2 FOREIGN KEY (project_id) REFERENCES project (id),
- CONSTRAINT assignment_ibfk_3 FOREIGN KEY (requested_from_manager_id) REFERENCES users (id),
- CONSTRAINT assignment_ibfk_4 FOREIGN KEY (requested_to_manager_id) REFERENCES users (id)
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `assignment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) NOT NULL,
+  `employee_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `requested_from_manager_id` int(11) NOT NULL,
+  `requested_to_manager_id` int(11) DEFAULT NULL,
+  `status` enum('Pending approval','Not approved','In progress','Done!') NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `employee_id` (`employee_id`),
+  KEY `project_id` (`project_id`),
+  KEY `requested_from_manager_id` (`requested_from_manager_id`),
+  KEY `requested_to_manager_id` (`requested_to_manager_id`),
+  CONSTRAINT `assignment_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `assignment_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`),
+  CONSTRAINT `assignment_ibfk_3` FOREIGN KEY (`requested_from_manager_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `assignment_ibfk_4` FOREIGN KEY (`requested_to_manager_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `assignment`
---
-
-LOCK TABLES `assignment` WRITE;
-/*!40000 ALTER TABLE `assignment` DISABLE KEYS */;
-/*!40000 ALTER TABLE `assignment` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `audit`
@@ -61,24 +54,15 @@ DROP TABLE IF EXISTS `audit`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `audit` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `employee_number` int(11) DEFAULT NULL,
-  `date_time` datetime DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `activity` varchar(200) DEFAULT NULL,
+  `employee_number` int(11) NOT NULL,
+  `date_time` datetime NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `activity` varchar(200) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `audit`
---
-
-LOCK TABLES `audit` WRITE;
-/*!40000 ALTER TABLE `audit` DISABLE KEYS */;
-/*!40000 ALTER TABLE `audit` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `country`
@@ -89,20 +73,10 @@ DROP TABLE IF EXISTS `country`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `country` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `country`
---
-
-LOCK TABLES `country` WRITE;
-/*!40000 ALTER TABLE `country` DISABLE KEYS */;
-INSERT INTO `country` VALUES (1,'Israel');
-/*!40000 ALTER TABLE `country` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `department`
@@ -113,19 +87,10 @@ DROP TABLE IF EXISTS `department`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `department` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `department`
---
-
-LOCK TABLES `department` WRITE;
-/*!40000 ALTER TABLE `department` DISABLE KEYS */;
-/*!40000 ALTER TABLE `department` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `employeeskill`
@@ -154,15 +119,6 @@ CREATE TABLE `employeeskill` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `employeeskill`
---
-
-LOCK TABLES `employeeskill` WRITE;
-/*!40000 ALTER TABLE `employeeskill` DISABLE KEYS */;
-/*!40000 ALTER TABLE `employeeskill` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `permissions`
 --
 
@@ -171,23 +127,10 @@ DROP TABLE IF EXISTS `permissions`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `permissions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  `role_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `role_id` (`role_id`),
-  CONSTRAINT `role_id` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `permissions`
---
-
-LOCK TABLES `permissions` WRITE;
-/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
-INSERT INTO `permissions` VALUES (1,'AddNewUser',1),(2,'UpdateUser',1),(3,'approveSkill',2),(4,'AddSkill',3);
-/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `project`
@@ -198,24 +141,16 @@ DROP TABLE IF EXISTS `project`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `project` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
   `manager_id` int(11) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `start_date` date DEFAULT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
   KEY `manager_id` (`manager_id`),
   CONSTRAINT `project_ibfk_1` FOREIGN KEY (`manager_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `project`
---
-
-LOCK TABLES `project` WRITE;
-/*!40000 ALTER TABLE `project` DISABLE KEYS */;
-/*!40000 ALTER TABLE `project` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `projectskill`
@@ -226,9 +161,9 @@ DROP TABLE IF EXISTS `projectskill`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `projectskill` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `project_id` int(11) DEFAULT NULL,
-  `skill_id` int(11) DEFAULT NULL,
-  `skill_level` int(11) DEFAULT NULL,
+  `project_id` int(11) NOT NULL,
+  `skill_id` int(11) NOT NULL,
+  `skill_level` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `project_id` (`project_id`),
   KEY `skill_id` (`skill_id`),
@@ -238,13 +173,22 @@ CREATE TABLE `projectskill` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `projectskill`
+-- Table structure for table `rolepermissions`
 --
 
-LOCK TABLES `projectskill` WRITE;
-/*!40000 ALTER TABLE `projectskill` DISABLE KEYS */;
-/*!40000 ALTER TABLE `projectskill` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `rolepermissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `rolepermissions` (
+  `role_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`role_id`,`permission_id`),
+  KEY `permission_id_idx` (`permission_id`),
+  KEY `role_permission_id` (`role_id`),
+  CONSTRAINT `permission_id` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`),
+  CONSTRAINT `role_permission_id` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `roles`
@@ -255,21 +199,12 @@ DROP TABLE IF EXISTS `roles`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
   `description` varchar(450) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `roles`
---
-
-LOCK TABLES `roles` WRITE;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'admin',NULL),(2,'manager',NULL),(3,'employee',NULL);
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `skills`
@@ -282,18 +217,10 @@ CREATE TABLE `skills` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `type` enum('TECHNICAL','PRODUCT') NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `skills`
---
-
-LOCK TABLES `skills` WRITE;
-/*!40000 ALTER TABLE `skills` DISABLE KEYS */;
-/*!40000 ALTER TABLE `skills` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `userrole`
@@ -313,15 +240,6 @@ CREATE TABLE `userrole` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `userrole`
---
-
-LOCK TABLES `userrole` WRITE;
-/*!40000 ALTER TABLE `userrole` DISABLE KEYS */;
-/*!40000 ALTER TABLE `userrole` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `users`
 --
 
@@ -330,33 +248,26 @@ DROP TABLE IF EXISTS `users`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `employee_number` int(11) DEFAULT NULL,
-  `first_name` text,
-  `last_name` text,
-  `email` text NOT NULL,
+  `employee_number` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `manager_id` int(11) DEFAULT NULL,
-  `department` text,
-  `work_site_id` int(11) DEFAULT NULL,
-  `country` text,
-  `phone` int(11) DEFAULT NULL,
+  `department` text NOT NULL,
+  `work_site_id` int(11) NOT NULL,
+  `country` text NOT NULL,
+  `phone` text,
   `login_status` bit(1) DEFAULT NULL,
-  `locked` bit(1) DEFAULT NULL,
-  `deactivated` bit(1) DEFAULT NULL,
-  `password` text,
+  `locked` bit(1) DEFAULT b'0',
+  `deactivated` bit(1) DEFAULT b'0',
+  `password` text NOT NULL,
+  `image` text,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
   KEY `work_site_id` (`work_site_id`),
   CONSTRAINT `users_ibfk_1` FOREIGN KEY (`work_site_id`) REFERENCES `worksite` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `worksite`
@@ -367,23 +278,14 @@ DROP TABLE IF EXISTS `worksite`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `worksite` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
   `country_id` int(11) NOT NULL,
-  `city` varchar(45) DEFAULT NULL,
+  `city` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `country_id` (`country_id`),
   CONSTRAINT `country_id` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `worksite`
---
-
-LOCK TABLES `worksite` WRITE;
-/*!40000 ALTER TABLE `worksite` DISABLE KEYS */;
-/*!40000 ALTER TABLE `worksite` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -394,15 +296,9 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-10 12:06:30
+-- Dump completed on 2019-09-20 14:04:42
 
 
-
-ALTER TABLE `assignments`.`users` 
-CHANGE COLUMN `phone` `phone` TEXT NULL DEFAULT NULL ;
-
-ALTER TABLE assignments.users
-ADD image text;
 
 INSERT INTO `assignments`.`worksite` (`name`, `country_id`, `city`) VALUES ('HiTech Park', '1', 'Nazareth');
 
@@ -410,43 +306,11 @@ INSERT INTO `assignments`.`users` (`employee_number`, `first_name`, `last_name`,
 VALUES ('1212', 'Shahar', 'Grauman', 'graumanoz@gmail.com', 'R&D', '1', 'Israel', '0541234567', b'1', b'0', b'0', '123456');
 
 INSERT INTO `assignments`.`users` (`employee_number`, `first_name`, `last_name`, `email`, `department`, `work_site_id`, `country`, `phone`, `login_status`, `locked`, `deactivated`, `password`) 
-VALUES ('1212', 'Shahar', 'Grauman', 'graumanoz@gmail.com', 'R&D', '1', 'Israel', '0541234567', b'1', b'0', b'0', '123456');
+VALUES ('1414', 'Shahar', 'Grauman', 'graumanoz@yahoo.com', 'R&D', '1', 'Israel', '0541234567', b'1', b'0', b'0', '123456');
 
 INSERT INTO `assignments`.`users` (`employee_number`, `first_name`, `last_name`, `email`, `manager_id`, `department`, `work_site_id`, `country`, `phone`, `login_status`, `locked`, `deactivated`, `password`) 
 VALUES ('3232', 'Majd', 'Rizik', 'majdrizik@gmail.com', '1', 'R&D', '1', 'Israel', '0541234568', b'1', b'0', b'0', '123456');
 
-
-ALTER TABLE `assignments`.`permissions` 
-DROP FOREIGN KEY role_id,
-DROP COLUMN `role_id`,
-DROP INDEX `role_id` ;
-ALTER TABLE project ADD UNIQUE (name);
-
-
-CREATE TABLE rolepermissions (
- role_permission_id int(11) NOT NULL,
- permission_id int(11) NOT NULL,
- KEY permission_id_idx (permission_id),
- KEY role_permission_id (role_permission_id),
- CONSTRAINT permission_id FOREIGN KEY (permission_id) REFERENCES permissions (id),
- CONSTRAINT role_permission_id FOREIGN KEY (role_permission_id) REFERENCES roles (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
---
--- Host: localhost    Database: admin
--- ------------------------------------------------------
--- Server version    8.0.16
-/!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT /;
-/!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS /;
-/!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION /;
-SET NAMES utf8 ;
-/!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE /;
-/!40103 SET TIME_ZONE='+00:00' /;
-/!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 /;
-/!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 /;
-/!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' /;
-/!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 /;
 --
 -- Dumping data for table assignment
 --
