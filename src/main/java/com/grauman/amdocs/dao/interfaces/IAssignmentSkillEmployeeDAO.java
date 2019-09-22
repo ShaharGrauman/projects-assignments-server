382bd6f30204b10ed42d314d7517ffbf8089fa70
Administrator <omair.issa6@gmail.com>
2019-09-22 13:28:35 +0300

    nothing

--- src/main/java/com/grauman/amdocs/dao/interfaces/IAssignmentSkillEmployeeDAO.java
+++ src/main/java/com/grauman/amdocs/dao/interfaces/IAssignmentSkillEmployeeDAO.java
@@ -7,10 +7,15 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface IAssignmentSkillEmployeeDAO extends IDAO<AssignmentSkillEmployeeVM> {
-    public List<AssignmentSkillEmployeeVM> getEmployeesByManagerID (Integer managerID, Integer currentPage, Integer limit) throws SQLException;
+    public List<AssignmentSkillEmployeeVM> getEmployeesByManagerID(Integer managerID, Integer currentPage, Integer limit) throws SQLException;
+
     public List<AssignmentSkillEmployeeVM> getEmployeesByProjectID(Integer projectID) throws SQLException;
+
     public List<AssignmentSkillEmployeeVM> getEmployeesByEmployeeName(String employeeName) throws SQLException;
+
     public List<AssignmentSkillEmployeeVM> searchEmployeesBySkillID(Integer skillID, Integer currentPage, Integer limit) throws SQLException;
-    public List<AssignmentSkillEmployeeVM> searchEmployeesBySkillSet(List<SkillsLevelVM> skillSet ,Integer currentPage,Integer limit) throws SQLException;
+
+    public List<AssignmentSkillEmployeeVM> searchEmployeesBySkillSet(List<SkillsLevelVM> skillSet, Integer currentPage, Integer limit) throws SQLException;
+
     public List<AssignmentSkillEmployeeVM> searchEmployeesBySkillName(String skillName, Integer pageNumber, Integer limit) throws SQLException;
 }
