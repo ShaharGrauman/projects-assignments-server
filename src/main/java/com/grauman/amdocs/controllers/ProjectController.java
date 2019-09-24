1f48598f06e6ead5b935c634d435e3649bbca2a9
rayah <rayah.khatib.2@gmail.com>
2019-09-24 15:13:00 +0300

    currentPage changed to page

--- src/main/java/com/grauman/amdocs/controllers/ProjectController.java
+++ src/main/java/com/grauman/amdocs/controllers/ProjectController.java
@@ -20,45 +20,53 @@ public class ProjectController {
     /**
      *
      * @param managerID
+     * @param page
+     * @param limit
      * @return list of projects that a manager employees are working on
      * @throws SQLException
      */
     @GetMapping("manager/{managerID}")
-    public ResponseEntity<List<ProjectVM>> getProjectsBymanagerID(@PathVariable("managerID") int managerID, @RequestParam int currentPage, @RequestParam int limit) throws SQLException {
-        return ResponseEntity.ok().body(projectsDAO.getProjectsByManagerID(managerID,currentPage,limit));
+    public ResponseEntity<List<ProjectVM>> getProjectsBymanagerID(@PathVariable("managerID") int managerID, @RequestParam int page, @RequestParam int limit) throws SQLException {
+        return ResponseEntity.ok().body(projectsDAO.getProjectsByManagerID(managerID,page,limit));
     }
 
     /**
      *
      * @param userID
+     * @param page
+     * @param limit
      * @return list of projects that an employee are working on by his/her ID
      * @throws SQLException
      */
     @GetMapping("user/{userID}")
-    public ResponseEntity<List<ProjectVM>> getProjectsByUserID(@PathVariable("userID") Integer userID, @RequestParam int currentPage, @RequestParam int limit) throws SQLException, ResultsNotFoundException {
-        return ResponseEntity.ok().body(projectsDAO.getProjectsByUserID(userID,currentPage,limit));
+    public ResponseEntity<List<ProjectVM>> getProjectsByUserID(@PathVariable("userID") Integer userID, @RequestParam int page, @RequestParam int limit) throws SQLException, ResultsNotFoundException {
+        return ResponseEntity.ok().body(projectsDAO.getProjectsByUserID(userID,page,limit));
     }
 
     /**
      *
      * @param userName
+     * @param page
+     * @param limit
      * @return list of projects that an employee are working on by his/her name
      * @throws SQLException
      */
     @GetMapping("user/name/{name}")
-    public ResponseEntity<List<ProjectVM>> getProjectsByUserName(@PathVariable("name") String userName, @RequestParam int currentPage, @RequestParam int limit) throws SQLException, ResultsNotFoundException {
-        return ResponseEntity.ok().body(projectsDAO.getProjectsByUserName(userName,currentPage,limit));
+    public ResponseEntity<List<ProjectVM>> getProjectsByUserName(@PathVariable("name") String userName, @RequestParam int page, @RequestParam int limit) throws SQLException, ResultsNotFoundException {
+        return ResponseEntity.ok().body(projectsDAO.getProjectsByUserName(userName,page,limit));
     }
 
     /**
      *
      * @param projectName
+     * @param page
+     * @param limit
      * @return search projects by name
      * @throws SQLException
      */
     @GetMapping("name/{name}")
-    public ResponseEntity<List<ProjectVM>> getProjectsByProjectName(@PathVariable("name") String projectName, @RequestParam int currentPage, @RequestParam int limit) throws SQLException {
-        return ResponseEntity.ok().body(projectsDAO.searchProjectByProjectName(projectName, currentPage, limit));
+    public ResponseEntity<List<ProjectVM>> getProjectsByProjectName(@PathVariable("name") String projectName, @RequestParam int page, @RequestParam int limit) throws SQLException {
+        return ResponseEntity.ok().body(projectsDAO.searchProjectByProjectName(projectName, page, limit));
     }
 
     /**
