d0e02213669dead2bb9fc079406bfefac55b0ef9
Lama8 <lama.azaiza@gmail.com>
2019-09-25 14:45:14 +0300

    inserting into audit from DAOs

--- src/main/java/com/grauman/amdocs/models/Audit.java
+++ src/main/java/com/grauman/amdocs/models/Audit.java
@@ -4,6 +4,7 @@ import java.sql.Date;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Audit {
 	private Integer id;
 	private Integer employeeNumber;
